cb28ad15a3cbd487dfdd1f244caf603298d8f621
Javier Veyrat <146321233+jVeyZ@users.noreply.github.com>
2024-09-08 19:35:42 +0200

    json updated data plus updater script using pupeteer

--- js/json_updater.js
+++ js/json_updater.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
-// Load the existing JSON data
+
 const jsonFilePath = path.join(__dirname, 'data.json');
 let jsonData = require(jsonFilePath);
 
