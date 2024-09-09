65749763435630441dd46125dbb6c576595c3206
Javier Veyrat <146321233+jVeyZ@users.noreply.github.com>
2024-09-09 13:18:41 +0200

    realigned divs and completed profiles page

---
+++ js/pfp-grid.js
@@ -0,0 +1,56 @@
+fetch('js/data.json')
+  .then(response => response.json()) 
+  .then(async jsonData => {
+    let items = jsonData.items;
+
+
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; 
+      }
+    }
+
+    shuffleArray(items); 
+
+    const outputDiv = document.getElementById('pfps');
+    if (!outputDiv) {
+      console.error('Element with ID "pfps" not found');
+      return;
+    }
+
+    for (const entry of items) {
+      if (!entry.tweet_url) {
+        console.error('No tweet URL found for entry', entry);
+        continue;
+      }
+
+      // Create the pfp 
+      const link = document.createElement('a');
+      link.href = entry.profile_url;
+      link.target = "_blank";
+      link.className = 'pfp';
+      link.style.backgroundImage = `url('${entry.pfp_url}')`; 
+
+      // Create the name 
+      const name = document.createElement('a');
+      name.href = entry.profile_url;
+      name.target = "_blank";
+      name.className = 'name';
+      name.textContent = entry.profile_name;
+
+      const block = document.createElement('div');
+      block.className = 'profile'
+  /*     block.href = entry.profile_url;
+      block.target = "_blank";
+      block.style.backgroundImage = `url('${entry.pfp_url}')`;  */
+
+      // Append the a tag to the output div
+      block.appendChild(link);
+      block.appendChild(name);
+      outputDiv.appendChild(block);
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching or processing data:', error);
+  });
